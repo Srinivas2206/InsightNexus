00fcd109fb035c1a8e407674148f7bf3a4d0b0ef
varshinitatikonda <153056927+varshinitatikonda@users.noreply.github.com>
2024-07-09 21:13:27 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,6 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 def preprocess_data(df):
+    """preprocess_data func will accept a single parameter df and processes the data and perform
+     feature selection and encodes the categorical features
+     parameter
+     -----------
+     df: pandas dataframe
+     returns
+     -------
+      encoded dataframe
+      """
     # Drop UserID column as we know it is not a significant column from EDA
     df = df.drop(columns=["UserID"])
     # Encode categorical features
@@ -16,6 +25,14 @@ def preprocess_data(df):
     return df, label_encoders
 
 def split_and_scale_data(df):
+    """ this func splits the dataset into train and test and scales the data seperately
+    parameters
+    ----------
+    df : pandas DataFrame
+    :returns
+    --------
+    pandas DataFrame  X_train, X_test, y_train, y_test
+    """
     # Let's define X and Y
     X = df.drop(columns=["CourseCompletion"])
     y = df["CourseCompletion"]
